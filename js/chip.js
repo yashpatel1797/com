29280eadcbcc0489ead44fef69566a5449dd7ff3
yashpatel1797 <yes.g.patel@gmail.com>
2022-02-06 14:58:32 +0530

    chip close

--- js/chip.js
+++ js/chip.js
@@ -41,43 +41,43 @@ for (let i = 0; i < c.length; i++) {
 
 // drag and drop
 // watch again to understan working - https://www.youtube.com/watch?v=jfYWwQrtzzY&ab_channel=WebDevSimplified
-const draggables = document.querySelectorAll('.draggable')
+// const draggables = document.querySelectorAll('.draggable')
 // const containers = document.querySelectorAll('.container')
-const container = document.querySelector('.drag-drop')
-draggables.forEach(draggable => {
-  draggable.addEventListener('dragstart', () => {
-    draggable.classList.add('dragging')
-  })
+// const container = document.querySelector('.drag-drop')
+// draggables.forEach(draggable => {
+//   draggable.addEventListener('dragstart', () => {
+//     draggable.classList.add('dragging')
+//   })
 
-  draggable.addEventListener('dragend', () => {
-    draggable.classList.remove('dragging')
-  })
-})
-
-// container.forEach(container => {
-  container.addEventListener('dragover', e => {
-    e.preventDefault()
-    const afterElement = getDragAfterElement(container, e.clientY)
-    const draggable = document.querySelector('.dragging')
-    if (afterElement == null) {
-      container.appendChild(draggable)
-    } else {
-      container.insertBefore(draggable, afterElement)
-    }
-  })
+//   draggable.addEventListener('dragend', () => {
+//     draggable.classList.remove('dragging')
+//   })
 // })
 
-function getDragAfterElement(container, y) {
-  const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+// container.forEach(container => {
+  // container.addEventListener('dragover', e => {
+  //   e.preventDefault()
+  //   const afterElement = getDragAfterElement(container, e.clientY)
+  //   const draggable = document.querySelector('.dragging')
+  //   if (afterElement == null) {
+  //     container.appendChild(draggable)
+  //   } else {
+  //     container.insertBefore(draggable, afterElement)
+  //   }
+  // })
+// })
 
-  return draggableElements.reduce((closest, child) => {
-    const box = child.getBoundingClientRect()
-    const offset = y - box.top - box.height / 2
-    if (offset < 0 && offset > closest.offset) {
-      return { offset: offset, element: child }
-    } else {
-      return closest
-    }
-  }, { offset: Number.NEGATIVE_INFINITY }).element
-}
+// function getDragAfterElement(container, y) {
+//   const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+
+//   return draggableElements.reduce((closest, child) => {
+//     const box = child.getBoundingClientRect()
+//     const offset = y - box.top - box.height / 2
+//     if (offset < 0 && offset > closest.offset) {
+//       return { offset: offset, element: child }
+//     } else {
+//       return closest
+//     }
+//   }, { offset: Number.NEGATIVE_INFINITY }).element
+// }
 }
